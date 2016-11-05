303be6916b5781e860dd71122b5466ab055c28f0
Kimberlyn Castro <kimberlyn_castro@yahoo.com>
2016-11-06 01:42:53 +0800

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>DL Divine | Home</title>
+
+  <!-- start of CSS links -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <!-- start of CSS links -->
+
+</head>
+<body>
+
+  <!-- start of navigation bar-->
+  <nav class="red darken-4" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo"><img class="responsive-img" src="img/DL.png" style="width: 220px; height: 110px;"></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="Rooms.php">Rooms</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
+        <li><a href="/updatedDLDivine12/admin/walkinbooking/booking.php">Book a Room</a></li>
+            <li><a href="dashboard.php">Dashboard</a></li>
+        <!-- Dropdown Trigger -->
+      <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Admin<i class="material-icons right">arrow_drop_down</i></a></li>
+       <!-- Dropdown Structure -->
+<ul id="dropdown1" class="dropdown-content">
+  <li><a href="acceptedusers.php">Profile Settings</a></li>
+      <li class="divider"></li>
+    <li><a href="logout.php">Logout</a></li>
+   
+    </ul>
+  
+</ul>
+      </ul>
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="index.php">Home</a></li>s
+        <li><a href="Rooms.php">Rooms</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
+
+       <!-- Dropdown Trigger -->
+      <li><a class="dropdown-button" href="#!" data-activates="dropdown2">Admin<i class="material-icons right">arrow_drop_down</i></a></li>
+       <!-- Dropdown Structure -->
+  <ul id='dropdown1' class='dropdown-content'>
+    <li><a href="announcements.php">Announcements</a></li>
+    <li class="divider"></li>
+    <li><a href="promos.php">Promos</a></li>
+    <li class="divider"></li>
+    <li><a href="activities.php">Activities</a></li>
+    <li class="divider"></li>
+    <li><a href="acceptedusers.php">Accepted Users</a></li>
+    <li class="divider"></li>
+    <li><a href="userinfo.php">Reservations</a></li>
+    <li class="divider"></li>
+     <li><a href="userinfo.php">List of Transactions</a></li>
+  </ul>
+
+      <a href="#" data-activates="nav-mobile" class="button-collapse" style="color:white"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+  <!-- end of navigation bar-->
+
+  <!-- start of DL DIVINE INN name and check in and out container -->
+  <div id="index-banner" class="parallax-container" style="height:600px;">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header center amber-text text-accent-3">DL Divine Inn</h1>
+        <h6 class="center">#61 P Valenzuela Street 1st Road, Salud Mitra Barangay, Baguio City</h6>
+         <center><img class="responsive-img" src="img/admin.png"><center>
+        <div class="row">
+          <div class="col s12 m3">
+            <div class="card grey darken-2">
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="img/pic3.jpg" alt="Unsplashed background img 1"></div>
+  </div><br>
+  <!-- start of DL DIVINE INN name and check in and out container -->
+
+  <hr>
+
+ 
+
+
+  <!-- start of the carousel -->
+  <div class="carousel">
+    <center><h4>Our Gallery</h4></center>
+    <a class="carousel-item" href="#one!"><img src="img/DL1.jpg" style="width:400px;"></a>
+    <a class="carousel-item" href="#two!"><img src="img/DL2.jpg" style="width:400px;"></a>
+    <a class="carousel-item" href="#three!"><img src="img/DL3.jpg" style="width:400px;"></a>
+    <a class="carousel-item" href="#four!"><img src="img/DL4.jpg" style="width:400px;"></a>
+    <a class="carousel-item" href="#five!"><img src="img/DL5.jpg" style="width:400px;"></a>
+  </div>
+  <!-- end of the carousel -->
+
+  <hr> 
+      
+  <!-- start of the contact details in DL DIVINE INN -->
+  <div class="container">
+    <div class="section">
+      <div class="row">
+        <div class="col s12 center">
+          <h3><i class="mdi-content-send brown-text"></i></h3>
+          <h4>Contact Us</h4>
+          <p class="center-align light">#61 P Valenzuela Street 1st Road, Salud Mitra Barangay,Baguio City Just Below SM and Barrio Fiesta</p>
+          <p class="center-align light">Tel. nos. (074) 442-3125 Main Office<br>
+          (074) 423-0900 Front Desk <br> Cell. No. 09107203239 Front Desk</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end of the contact details in DL DIVINE INN -->
+
+  <!-- start of footer -->
+  <footer class="page-footer red darken-4">
+    <div class="footer-copyright">
+      <div class="container">
+        Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
+  <!-- end of footer -->
+
+  <!-- start of Scripts -->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+  <script src="js/modal.js"></script>
+  <script src="js/dropdown.js"></script>
+  <!-- end of Scripts -->
+
+  </body>
+</html>
---
+++ admin/promos.php
@@ -0,0 +1,223 @@
+<?php
+
+
+
+include("sessionadmin.php");
+// php code to Update data from mysql database Table
+include("connect.php");
+
+
+if(isset($_POST['Post']))
+{
+
+$hostname = "localhost";
+   $username = "root";
+   $password = "";
+   $databaseName = "accounts";
+   
+   $connect = mysqli_connect($hostname, $username, $password, $databaseName);
+   
+   // get values form input text and number
+   $promos = $_POST['promos'];
+   
+   // get values form input text and number
+   $date = $_POST['date'];
+
+   // mysql query to Update data
+
+
+   $query = "INSERT INTO `promos` (`promos`,`date`) VALUES ('$promos', '$date')";
+   
+
+   $result = mysqli_query($connect, $query);
+
+
+   
+   if($result)
+   {
+      echo "<script>alert('Promos Posted')</script>";
+   }else{
+       echo "<script>alert('Error')</script>";
+   }
+
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>DL Divine | Promos</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+<div class="page-wrap">
+  <nav class="red darken-4" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo"><img class="responsive-img" src="img/DL.png" style="width: 100px; height: 60px;"></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="index.php">Home</a></li>
+          <li><a href="rooms.php">Rooms</a></li>
+           <li><a href="gallery.php">Gallery</a></li>
+   
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <!-- Dropdown Trigger -->
+      <li><a class="dropdown-button" href="#!" data-activates="dropdown2">Admin<i class="material-icons right">arrow_drop_down</i></a></li>
+
+
+           <!-- Dropdown Structure -->
+          <ul id="dropdown2" class="dropdown-content">
+            <li><a href="acceptedusers.php">Accepted Users</a></li>
+           <li class="divider"></li>
+          <li><a href="userinfo.php">Reservations</a></li>
+           <li class="divider"></li>
+          <li><a href="userinfo.php">List of Transactions</a></li>
+           <li class="divider"></li>
+          <li><a href="logout.php">Logout</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="index.php">Home</a></li>
+          <li><a href="#">Rooms</a></li>
+           <li><a href="gallery.php">Gallery</a></li>
+            <li><a href="login.php">Logout</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse" style="color:white"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+
+</head>
+<style>
+html, body {
+  height: 100%;
+}
+.page-wrap {
+  min-height: 100%;
+  /* equal to footer height */
+  margin-bottom: -142px; 
+}
+.page-wrap:after {
+  content: "";
+  display: block;
+}
+.site-footer, .page-wrap:after {
+  height: 142px; 
+}
+.site-footer: {
+background-color: rgb(183, 28, 28);
+}
+
+</style>
+<body>
+<br>
+ <div class="row">  
+ <div class="col s3">
+ <br>
+ <!-- Dropdown Trigger -->
+  <center><a class=' red darken-4 dropdown-button btn' href='#' data-activates='dropdown1' style="width:150px;">Select</a></center>
+
+  <!-- Dropdown Structure -->
+  <ul id='dropdown1' class='dropdown-content'>
+    <li><a href="announcements.php">Announcements</a></li>
+    <li class="divider"></li>
+    <li><a href="promos.php">Promos</a></li>
+    <li class="divider"></li>
+    <li><a href="activities.php">Activities</a></li>
+  </ul>
+</div>
+
+ <div class="col s9">
+
+
+
+
+<div class = "row">
+<form class = "col s12"  action="promos.php" method = "post">
+<div class = "row">
+<div class = "input-field col s12 ">
+<h5>Post new promos here! </h4>
+<textarea  id= "textarea1"  name = "promos" class = "materialize-textarea" required=""></textarea>
+
+<input class="green waves-effect waves-light btn" style="width:140px; " type="submit" name="Post" value ="Post" >
+
+<input  style=" float:right;  border:none; background:transparent;"  name="date" value =" <?php
+echo " " . date("Y/m/d") . " - " . date("l");
+?>" readonly>
+
+
+  <br>
+  
+
+                         
+</div>
+</form>
+</div>
+
+
+
+
+
+
+
+
+ 
+
+</div>
+</div>
+  
+</div>
+</div>
+  <footer class="page-footer red darken-4">
+  <!--
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+--> 
+    <div class="site-footer">
+      <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+  <script src="js/modal.js"></script>
+
+</body>
+</html>
+
+  
